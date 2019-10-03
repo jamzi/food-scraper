42bd02583a7378f4dea469bc404996506acba5d3
Janez Cadez <janez.cadez007@gmail.com>
2019-10-03 06:01:12 +0200

    cleanup

--- constants/restaurants.js
+++ constants/restaurants.js
@@ -2,55 +2,46 @@ export default [
   {
     id: "vinka",
     name: "Vinka",
-    url: "https://www.vinka.si/malice-in-kosila.html",
-    menuItems: undefined
+    url: "https://www.vinka.si/malice-in-kosila.html"
   },
   {
     id: "gastro",
     name: "Gastro House 151",
-    url: "https://www.gastrohouse.si/index.php/tedenska-ponudba",
-    menuItems: undefined
+    url: "https://www.gastrohouse.si/index.php/tedenska-ponudba"
   },
   {
     id: "restavracija123",
     name: "Restavracija 123",
-    url: "https://www.restavracija123.si/?restaurantid=213",
-    menuItems: undefined
+    url: "https://www.restavracija123.si/?restaurantid=213"
   },
   {
     id: "barbado",
     name: "Barbado",
-    url: "http://www.barbado.si/",
-    menuItems: undefined
+    url: "http://www.barbado.si/"
   },
   {
     id: "piap",
     name: "Piap",
-    url: "http://www.piap.si/jedilnik",
-    menuItems: undefined
+    url: "http://www.piap.si/jedilnik"
   },
   {
     id: "favola",
     name: "Favola",
-    url: "http://www.kaval-group.si/FAVOLA,,ponudba/kosila",
-    menuItems: undefined
+    url: "http://www.kaval-group.si/FAVOLA,,ponudba/kosila"
   },
   {
     id: "rozaSlon",
     name: "Roza Slon",
-    url: "http://www.rozaslon.si/ponudba/",
-    menuItems: undefined
+    url: "http://www.rozaslon.si/ponudba/"
   },
   {
     id: "gostilna1987",
     name: "Gostilna 1987",
-    url: "https://gostilna1987.si/",
-    menuItems: undefined
+    url: "https://gostilna1987.si/"
   },
   {
     id: "vivo",
     name: "VIVO D125",
-    url: "https://www.vivo.si/vivo-d125-jedilnik/",
-    menuItems: undefined
+    url: "https://www.vivo.si/vivo-d125-jedilnik/"
   }
 ];
